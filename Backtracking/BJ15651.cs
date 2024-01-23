dd1a588ab63b375e72f7bf9f1ab2a46cae1a6def
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-01-23 11:47:08 +0900

    feat : 백준 7576, 15651 풀이

---
+++ Backtracking/BJ15651.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Backtracking
+{
+    // https://www.acmicpc.net/problem/15651
+    class BJ15651
+    {
+        static void Main()
+        {
+            // 입력값 파싱
+            string[] args = Console.ReadLine().Split();
+            int N = int.Parse(args[0]);
+            int M = int.Parse(args[1]);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= N; i++)
+            {
+                sb.Append(i);
+            }
+            string nums = sb.ToString();
+            sb.Clear();
+
+            // DFS로 모든 순열 찾고 스트링빌더에 삽입후 출력
+            dfs("", 0, nums, M, sb);
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void dfs(string cur, int count, string nums, int len, StringBuilder sb)
+        {
+            if (count == len)
+            {
+                sb.Append(cur);
+                sb.Append("\n");
+                return;
+            }
+            foreach (char n in nums)
+            {
+                StringBuilder temp = new StringBuilder(cur);
+                temp.Append(n);
+                temp.Append(" ");
+                dfs(temp.ToString(), count + 1, nums, len, sb);
+            }
+        }
+    }
+}
---
+++ GraphSearch/BJ7576.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSearch
+{
+    // https://www.acmicpc.net/problem/7576
+
+    class BJ7576
+    {
+        static void Main()
+        {
+            string[] args = Console.ReadLine().Split();
+            int C = int.Parse(args[0]);
+            int R = int.Parse(args[1]);
+
+            int[,] farm = new int[R, C];
+            Queue<int[]> ripes = new Queue<int[]>();
+
+            // 익은 토마토를 큐에 보관
+            for (int r = 0; r < R; r++)
+            {
+                string[] row = Console.ReadLine().Split();
+                for (int c = 0; c < C; c++)
+                {
+                    farm[r, c] = int.Parse(row[c]);
+                    if (farm[r, c] == 1)
+                    {
+                        ripes.Enqueue(new int[] { r, c });
+                    }
+                }
+            }
+
+            int days = -1;
+            // 이미 익은 토마토마다 옆에 있는 토마토 익히기
+            while (ripes.Count > 0)
+            {
+                List<int[]> today = new List<int[]>();
+                days++;
+                while (ripes.Count > 0)
+                {
+                    today.Add(ripes.Dequeue());
+                }
+                foreach (var ripe in today)
+                {
+                    int r = ripe[0];
+                    int c = ripe[1];
+                    if (r > 0) //Up
+                    {
+                        if (farm[r - 1, c] == 0)
+                        {
+                            farm[r - 1, c] = 1;
+                            ripes.Enqueue(new int[] { r - 1, c });
+                        }
+                    }
+                    if (r < R - 1) //Down
+                    {
+                        if (farm[r + 1, c] == 0)
+                        {
+                            farm[r + 1, c] = 1;
+                            ripes.Enqueue(new int[] { r + 1, c });
+                        }
+                    }
+                    if (c > 0) //Left
+                    {
+                        if (farm[r, c - 1] == 0)
+                        {
+                            farm[r, c - 1] = 1;
+                            ripes.Enqueue(new int[] { r, c - 1 });
+                        }
+                    }
+                    if (c < C - 1) //Right
+                    {
+                        if (farm[r, c + 1] == 0)
+                        {
+                            farm[r, c + 1] = 1;
+                            ripes.Enqueue(new int[] { r, c + 1 });
+                        }
+                    }
+                }
+            }
+
+            //안익은 토마토가 있는지 확인
+            bool unripeExists = false;
+            for (int r = 0; r < R; r++)
+            {
+                for (int c = 0; c < C; c++)
+                {
+                    if (farm[r, c] == 0)
+                    {
+                        unripeExists = true;
+                        break;
+                    }
+                }
+                if (unripeExists)
+                {
+                    break;
+                }
+            }
+
+            if (unripeExists)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(days);
+            }
+        }
+    }
+}
